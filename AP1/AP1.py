ead405c852f60c9e8a59eebf7bb2c53de9564d9b
Koding <jsu3848j@users.noreply.github.com>
2015-10-01 01:01:57 +0000

    AP1

---
+++ AP1/AP1.py
@@ -0,0 +1,11 @@
+# imports
+import pandas as pd
+import seaborn as sns
+import statsmodels.formula.api as smf
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.cross_validation import train_test_split
+import numpy as np
+
+# allow plots to appear directly in the notebook
+%matplotlib inline
